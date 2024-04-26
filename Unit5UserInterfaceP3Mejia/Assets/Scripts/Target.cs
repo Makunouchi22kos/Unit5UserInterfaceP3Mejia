a0b814ffe150f8fde84fed805105f6781d207e8e
Allan Mejia-Mora <723495@student.dusd.net>
2024-04-26 11:42:31 -0700

    Gamerover

--- Unit5UserInterfaceP3Mejia/Assets/Scripts/Target.cs
+++ Unit5UserInterfaceP3Mejia/Assets/Scripts/Target.cs
@@ -10,6 +10,9 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,8 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(),  RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos(); 
+        gameManager = GameObject.Find("Game Manager")
+            .GetComponent<GameManager>();
     }
 
     Vector3 RandomForce()
@@ -34,6 +39,27 @@ public class Target : MonoBehaviour
     Vector3 RandomSpawnPos ()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position,
+                explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
     // Update is called once per frame
